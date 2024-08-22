496aac37ccab1b30cd4d7d9dfbe90df37c824d15
Ebukaenyinnaya7 <ebukaenyinnaya7@gmail.com>
2024-08-22 02:52:32 -0800

    Add files via upload

---
+++ js/client.js
@@ -0,0 +1,11 @@
+
+      (function() {
+          window.fueledConfig = {"frontendStorageUrl":"https://frontend-storage.fueled.io","datahubUrl":"https://datahub.fueled.io","googleTagManagerUrl":"https://www.googletagmanager.com/gtag/js","respectClientTracking":true,"firstPartyDataEnabled":false,"captureClientEvents":false,"addToCartTracking":true,"contactPersonEmail":"marketing@supermartng.com","analyticsWriteKey":"YF3Pcz6EWyp3SM4dAgS8hHuqV16jKs","ga4":{"gTarget":"G-J8632CEQ68","valueField":"total"},"shopifyTracking":true};
+        
+          const script = document.createElement("script");
+          script.src = 'https://cdn-client.fueled.io/shopify-fueled-client-2.45.0.min.js';
+          document.head.appendChild(script);
+          
+           
+      })();
+    
